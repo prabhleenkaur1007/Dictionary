58cc19d069223f285aaf6959de39d378b8bba36a
prabhleenkaur1007 <43867458+prabhleenkaur1007@users.noreply.github.com>
2019-01-19 23:44:54 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,11 @@
-# Getting a little tricker
+1))# Getting a little tricker
 d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
 # This was harder than I expected...
 print (d['k1'][0]['nest_key'][1][0])
 
 
-# This will be hard and annoying!
+2))# This will be hard and annoying!
 d1 = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
 
 # Phew
